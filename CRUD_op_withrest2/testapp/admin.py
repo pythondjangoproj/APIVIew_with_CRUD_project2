91050ee99e4910a0ef0b10000b713b214ba1f277
vishal.python10@gmail.com <vishal.python10@gmail.com>
2022-03-23 18:45:56 +0530

    first commit

--- CRUD_op_withrest2/testapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Employee
-# Register your models here.
-
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['id', 'eno', 'ename', 'esal', 'eaddr']
-
-
-admin.site.register(Employee, EmployeeAdmin)
